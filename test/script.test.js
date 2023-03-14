d4c438802400ff1702d8c7ad490066e5cfe9792d
TayneB <tayne.bilyard@developersinstitute.co.nz>
2023-03-14 15:39:34 +1300

    Test 2 Failing successfully

--- test/script.test.js
+++ test/script.test.js
@@ -12,11 +12,24 @@ beforeEach(() => {
 
 describe("getBotReply", () => {
   it("should have correct response for George", () => {
+    // Input: George
     const botReply1 = getBotReply("George");
+    // Output: Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?
+    const expectedReply1 = "Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?";
+    expect(botReply1).toEqual(expectedReply1);
+  });
+  it("should have correct response for George, no", () => {
+    // Input: George
+    const botReply1 = getBotReply("George");
+    // Output: Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?
     const expectedReply1 = "Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?";
     expect(botReply1).toEqual(expectedReply1);
 
-    // Uncomment the following line and update your expectation
-    // expect(botReply1).toEqual(expectedReply1);
+    // Input: no
+    const botReply2 = getBotReply('no');
+    // Output: A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?
+    const expectedReply2 = 'A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?';
+    expect(botReply2).toEqual(expectedReply2);
+
   });
 });
