a0ec93cb3a51b68d9a6d369ca2d61dcceb5723c6
TayneB <tayne.bilyard@developersinstitute.co.nz>
2023-03-14 17:31:08 +1300

    Timeloop testing begin

--- Submission/script.js
+++ Submission/script.js
@@ -8,13 +8,20 @@
 let step = 1;
 const getBotReply = (msg) => {
   if (step === 1) {
-    step += 1;
+    step = 2;
   return `Well met, ${msg}, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?`;
   };
   if (step === 2) {
+    step = 3;
     return 'A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?'; 
-
   };
+  if (step === 3) {
+    return `Well if you'd like to know more about dnd you can check out more info here. You could also tell me to "Timeloop" and we can start again. Try not to restart time too much, or things get... tricky.`;
+  };
+  if (msg === 'timeloop') {
+    return `Greetings traveler, I am Yetan. What do you call yourself?`;
+  }
+
 };
 
 export { getBotReply };
--- test/script.test.js
+++ test/script.test.js
@@ -18,7 +18,7 @@ describe("getBotReply", () => {
     const expectedReply1 = "Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?";
     expect(botReply1).toEqual(expectedReply1);
   });
-  
+
   it("should have correct response for George, no", () => {
     // Input: George
     const botReply1 = getBotReply("George");
@@ -31,6 +31,56 @@ describe("getBotReply", () => {
     // Output: A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?
     const expectedReply2 = 'A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?';
     expect(botReply2).toEqual(expectedReply2);
+
+  });
+
+  it("should have correct response for George, no", () => {
+    // Input: George
+    const botReply1 = getBotReply("George");
+    // Output: Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?
+    const expectedReply1 = "Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?";
+    expect(botReply1).toEqual(expectedReply1);
+
+    // Input: no
+    const botReply2 = getBotReply('no');
+    // Output: A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?
+    const expectedReply2 = 'A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?';
+    expect(botReply2).toEqual(expectedReply2);
+
+    // Input: no
+    const botReply3 = getBotReply('no');
+    // Output: Well if you'd like to know more about dnd you can check out more info here. You could also tell me to "Timeloop" and we can start again. Try not to restart time too much, or things get... tricky.
+    const expectedReply3 = `Well if you'd like to know more about dnd you can check out more info here. You could also tell me to "Timeloop" and we can start again. Try not to restart time too much, or things get... tricky.`;
+    expect(botReply3).toEqual(expectedReply3);
+    
+
+  });
+
+  it("should have correct response for George, no", () => {
+    // Input: George
+    const botReply1 = getBotReply("George");
+    // Output: Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?
+    const expectedReply1 = "Well met, George, I am here to help you decide your destiny within the world of Faerun or beyond. Are you familiar with Dungeons and Dragons 5th edition?";
+    expect(botReply1).toEqual(expectedReply1);
+
+    // Input: no
+    const botReply2 = getBotReply('no');
+    // Output: A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?
+    const expectedReply2 = 'A newcomer! Let me explain, Dungeons and Dragons is a roleplaying game that has you act as an adventurer whom you create to play as with a group of friends to overcome challenges set for your group by the game master. The outcome of actions within the game are determined by dice rolls and your characters abilities. We are going to look at what classes from this game might interest you based on your preferences or what sounds cool! Would like to continue determining your fate?';
+    expect(botReply2).toEqual(expectedReply2);
+
+    // Input: no
+    const botReply3 = getBotReply('no');
+    // Output: Well if you'd like to know more about dnd you can check out more info here. You could also tell me to "Timeloop" and we can start again. Try not to restart time too much, or things get... tricky.
+    const expectedReply3 = `Well if you'd like to know more about dnd you can check out more info here. You could also tell me to "Timeloop" and we can start again. Try not to restart time too much, or things get... tricky.`;
+    expect(botReply3).toEqual(expectedReply3);
+    
+    // Input: no
+    const botReply4 = getBotReply('timeloop');
+    // Output: Well if you'd like to know more about dnd you can check out more info here. You could also tell me to "Timeloop" and we can start again. Try not to restart time too much, or things get... tricky.
+    const expectedReply4 = `Greetings traveler, I am Yetan. What do you call yourself?`;
+    expect(botReply4).toEqual(expectedReply4);
+    
 
   });
 });
